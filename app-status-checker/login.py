93efda5ec7be972552c5cbefb8fd3d4e93f4eaf9
cal <caldouful@gmail.com>
2023-01-01 13:13:04 -0700

    "adding files" "adding files"

---
+++ app-status-checker/login.py
@@ -0,0 +1,21 @@
+import requests
+
+# Set the URL of the login form and the URL to redirect to after login
+login_url = 'https://pitneybowes.wd1.myworkdayjobs.com/en-US/PBCareers/login?redirect=%2Fen-US%2FPBCareers%2FuserHome'
+redirect_url = 'https://pitneybowes.wd1.myworkdayjobs.com/en-US/PBCareers/userHome'
+
+# Set the login credentials
+data = {
+    'Email Address': 'calebdouglasfuller@gmail.com',
+    'Password': 'LehighSWE#23'
+}
+
+# Send the POST request to the login form
+response = requests.post(login_url, data=data)
+
+# Check if the login was successful by checking the response status code
+# and the URL of the response
+if response.status_code == 200 and response.url == redirect_url:
+    print('Login successful!')
+else:
+    print('Login failed')
---
+++ nba-projects/starter_project_1.py
@@ -0,0 +1,7 @@
+# First we will import our packages
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
+import requests
+from nba_api.stats import endpoints
+from matplotlib import pyplot as plt
