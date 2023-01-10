841e0199fff51cb6f000f910be92b8a16f113817
cal <caldouful@gmail.com>
2023-01-10 14:35:01 -0500

    new project

---
+++ nba-projects/ape-index-analyzer/ap_scraper.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
+import requests
+from nba_api.stats import endpoints
+from matplotlib import pyplot as plt
+from bs4 import BeautifulSoup
