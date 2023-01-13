2640b87a8cc1bc641ed045cdecc68166654de7a7
cal <caldouful@gmail.com>
2023-01-12 20:54:27 -0500

    added html form

--- lyric-finder/src/Lyrics.js
+++ lyric-finder/src/Lyrics.js
@@ -7,3 +7,25 @@ function Lyrics() {
     const [artist, setArtist] = useState('');
     const [title, setTitle] = useState('');
 }
+
+<form>
+    <label>
+        Song Artist:
+        <input
+            type="text"
+            value={songArtist}
+            onChange={e => setSongArtist(e.target.value)}
+        />
+    </label>
+    <label>
+        Song Title:
+        <input
+            type="text"
+            value={songTitle}
+            onChange={e => setSongTitle(e.target.value)}
+        />
+    </label>
+    <button type="submit" onClick={fetchLyrics}>
+        Fetch Lyrics
+    </button>
+</form>
