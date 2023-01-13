2a3676d7ab9ee1683489a01b5b6db6e64e5d7fa7
cal <caldouful@gmail.com>
2023-01-12 20:50:27 -0500

    building frontend to application

---
+++ lyric-finder/src/Lyrics.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+function Lyrics() {
+    const [songArtist, setSongArtist] = useState('');
+    const [songTitle, setSongTitle] = useState('');
+    const [lyrics, setLyrics] = useState('');
+    const [artist, setArtist] = useState('');
+    const [title, setTitle] = useState('');
+}
