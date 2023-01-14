07051ae7803edc05bdfb227755cee88b26fd9ac3
cal <caldouful@gmail.com>
2023-01-14 16:49:16 -0500

    rewriting server to use selenium as js doesnt support beautifulsoup

--- lyric-finder/src/server.js
+++ lyric-finder/src/server.js
@@ -1,38 +1,25 @@
-const requests = require('request-promise');
-const BeautifulSoup = require('beautifulsoup4').default;
 const express = require("express");
-const path = require("path");
+const {Builder, By, Key, until} = require('selenium-webdriver');
 const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-//app.get("/", (req, res) => {
-//    res.send("Server is running!");
-//});
-
-app.get("/", (req, res) => {
-    res.redirect("/lyrics");
-});
 
 app.get("/lyrics/:artist/:title", async (req, res) => {
     const { artist, title } = req.params;
-    // code to scrape the lyrics, artist and title with BeautifulSoup and return it JSON
-    // for example:
     try {
         const songArtist = artist;
         const songTitle = title;
-
-        URL = "https://www.azlyrics.com/lyrics/" + songArtist + "/" + songTitle + ".html";
-        page = await requests.get(URL);
-        if (page.status_code !== 200)
-            return res.status(404).json({error: "lyrics not found"});
-        soup = BeautifulSoup(page.content, "html.parser");
-
-        artist = soup.find('div', class_ = 'lyricsh').h2.text;
-        title = soup.find('div', class_ = 'col-xs-12 col-lg-8 text-center').find_all('div',class_= 'div-share')[1].text.split('"')[1];
-        lyrics = soup.find("div", class_="col-xs-12 col-lg-8 text-center").find_all("div")[5].text;
+        
+        // Create a new instance of a web driver
+        let driver = await new Builder().forBrowser('chrome').build();
+        // Go to the website
+        await driver.get(`https://www.azlyrics.com/lyrics/${songArtist}/${songTitle}.html`);
+        // Get the lyrics and artist and title
+        let lyrics = await driver.findElement(By.xpath("//div[@class='col-xs-12 col-lg-8 text-center']//div[5]")).getText();
+        let artist = await driver.findElement(By.xpath("//div[@class='lyricsh']//h2")).getText();
+        let title = await driver.findElement(By.xpath("//div[@class='col-xs-12 col-lg-8 text-center']//div[2]")).getText().split('"')[1];
+        //Return the lyrics, artist and title as json
         res.json({artist, title, lyrics});
+        // close the browser
+        await driver.quit();
     } catch (error) {
         console.log(error);
         res.status(500).json({error: "Internal server error"});
@@ -42,3 +29,4 @@ app.get("/lyrics/:artist/:title", async (req, res) => {
 app.listen(8000, () => {
     console.log("Server started on http://localhost:8000");
 });
+
